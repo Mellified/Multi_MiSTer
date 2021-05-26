1d01698dd9f01efbac905b3b8725e5d1a3e4e7f9
Mellified <81110968+Mellified@users.noreply.github.com>
2021-05-26 11:38:04 -0500

    P3/P4 support

---
+++ Scripts/linux_p4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Set system configuration to known values for Player 2 MiSTer
+
+echo "Remounting root filesystem as read-write..."
+mount | grep "on / .*[(,]ro[,$]" -q && RO_ROOT="true"
+[ "${RO_ROOT}" == "true" ] && mount / -o remount,rw
+echo "Copying identity files..."
+cp /media/fat/linux/hosts_p2 /etc/hosts
+cp /media/fat/linux/hostname_p2 /etc/hostname
+cp /media/fat/linux/u-boot.txt_p2 /media/fat/linux/u-boot.txt
+echo "Remounting root filesystem as read-only..."
+sync
+[ "${RO_ROOT}" == "true" ] && mount / -o remount,ro
+sync
+echo "Done!"
+echo "Rebooting in..."
+for i in {5..1}; do
+	echo "${i} seconds"
+	sleep 1
+done
+reboot now
+exit 0
